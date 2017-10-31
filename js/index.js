8a9714caca6b8e1efdfadf009d849014e30fcd41
Alex <kokomini@qq.com>
2017-10-31 16:40:19 +0800

    升级提示框

--- js/index.js
+++ js/index.js
@@ -80,6 +80,20 @@ var customCheckbox = {
     }
 }
 
+var loader = {
+    template: `<div class="loader loader--style" title="0">
+    <svg version="1.1" id="loader" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
+    <path opacity="0.2" fill="#fff" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+    s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
+    c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z" />
+    <path fill="#fff" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+    C22.32,8.481,24.301,9.057,26.013,10.047z">
+    <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="0.5s" repeatCount="indefinite" />
+    </path>
+    </svg>
+    </div>`
+}
+
 let storedActions = getStore('actions');
 let defaultActions = [
     {
@@ -354,6 +368,12 @@ var app = new Vue({
         loadingMsg: '处理中...',
         isDone: false,
         message: '处理成功',
+        needUpdating: true, // 显示升级提示
+        newVersion: {
+            current: '1.0.0',
+            latest: '1.1.0',
+            url: 'https://github.com/SuperAL/splice/releases/'
+        },
         clear: null, // clear setTimeout
         currentStatus: '',
         isSolo: false, // 当前只有一个操作，不能拼接其他操作
@@ -372,7 +392,8 @@ var app = new Vue({
     components: {
         customInput,
         customRadio,
-        customCheckbox
+        customCheckbox,
+        loader
 
     },
     watch: {
@@ -473,6 +494,10 @@ var app = new Vue({
         closeLoading() {
             this.manually = true;
             this.isLoading = false;
+        },
+        closeUpdating() {
+            this.manually = true;
+            this.needUpdating = false;
         }
     }
 })
